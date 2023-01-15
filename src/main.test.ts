b31c80bcf428a8714ef3a521bdf84a54c980e286
Ümit Tunç <umitreva@gmail.com>
2023-01-15 16:48:00 +0300

    Asset Equals has been created for the other four operations math functions.

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,9 +1,32 @@
 import { assertEquals } from "@deno-std/testing/asserts.ts";
 import { describe, it } from "@deno-std/testing/bdd.ts";
 import { add } from "./main.ts";
+import { subtract } from "./main.ts";
+import { divide } from "./main.ts";
+import { multiply } from "./main.ts";
 
 describe("add", () => {
   it("basic", () => {
     assertEquals(add(2, 3), 5);
   });
 });
+
+describe("subtract", () => {
+  it("basic", () => {
+    assertEquals(subtract(2, 3), 5);
+  });
+});
+
+describe("divide", () => {
+  it("basic", () => {
+    assertEquals(divide(2, 3), 5);
+  });
+});
+
+describe("multiply", () => {
+  it("basic", () => {
+    assertEquals(multiply(2, 3), 5);
+  });
+});
+
+
