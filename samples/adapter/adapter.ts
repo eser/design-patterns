2857b8b7388a13faa6ea1fec8d04bf737be44888
Gokhan <gokhanhkocyigit@gmail.com>
2023-01-17 22:45:20 +0300

    add adapter sample for hypothetical payment service

---
+++ samples/adapter/adapter.ts
@@ -0,0 +1,23 @@
+import {Payment,PaymentService} from '@samples/adapter/old.ts';
+import {Transaction} from '@samples/adapter/new.ts';
+
+class TransactionPaymentAdatper implements Payment{
+    constructor(private transaction: Transaction){}
+    pay(amount: number): void {
+        this.transaction.newTypePay(amount);
+    }
+
+}
+
+const main = (): void => {
+    const service = new PaymentService();
+    const transaction = new Transaction();
+
+    service.createPayment(new TransactionPaymentAdatper(transaction));
+};
+
+if (import.meta.main) {
+    main();
+}
+
+export { TransactionPaymentAdatper }
---
+++ samples/adapter/old.ts
@@ -0,0 +1,14 @@
+// This is the old code that we don't want to (or can't) change
+class Payment{
+    pay(amount: number){
+        console.log(`old type pay ${amount}`);
+    }
+}
+
+class PaymentService{
+    createPayment(payment: Payment):void {
+        payment.pay(100);
+    }
+}
+
+export {PaymentService, type Payment};
