5bdf7f052412d9931032b6e70874e87d64ba5371
Gokhan <gokhanhkocyigit@gmail.com>
2023-01-17 22:53:25 +0300

    update the format

--- samples/adapter/adapter.ts
+++ samples/adapter/adapter.ts
@@ -1,23 +1,22 @@
-import {Payment,PaymentService} from '@samples/adapter/old.ts';
-import {Transaction} from '@samples/adapter/new.ts';
+import { Payment, PaymentService } from "@samples/adapter/old.ts";
+import { Transaction } from "@samples/adapter/new.ts";
 
-class TransactionPaymentAdatper implements Payment{
-    constructor(private transaction: Transaction){}
-    pay(amount: number): void {
-        this.transaction.newTypePay(amount);
-    }
-
+class TransactionPaymentAdatper implements Payment {
+  constructor(private transaction: Transaction) {}
+  pay(amount: number): void {
+    this.transaction.newTypePay(amount);
+  }
 }
 
 const main = (): void => {
-    const service = new PaymentService();
-    const transaction = new Transaction();
+  const service = new PaymentService();
+  const transaction = new Transaction();
 
-    service.createPayment(new TransactionPaymentAdatper(transaction));
+  service.createPayment(new TransactionPaymentAdatper(transaction));
 };
 
 if (import.meta.main) {
-    main();
+  main();
 }
 
-export { TransactionPaymentAdatper }
+export { TransactionPaymentAdatper };
--- samples/adapter/new.ts
+++ samples/adapter/new.ts
@@ -1,8 +1,8 @@
 //This is the new code that we want to use with old payment service
-class Transaction{
-    newTypePay(amount: number){
-        console.log(`new type pay ${amount}`);
-    }
+class Transaction {
+  newTypePay(amount: number) {
+    console.log(`new type pay ${amount}`);
+  }
 }
 
-export {Transaction};
+export { Transaction };
